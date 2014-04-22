38fb80718284db1e0ba1742f06a2d7022a1824c9
Gatot Ari Wibowo <gawibowo@gmail.com>
2014-04-22 17:45:58 +0700

    add columns, buttons, lists, message boxes

--- buttons2.js
+++ buttons2.js
@@ -1,4 +1,4 @@
-(function() {
+(function($) {
 	tinymce.PluginManager.add('g7_button', function(editor, url) {
 		editor.addButton('sc_button', {
 			title: 'Shortcodes',
@@ -37,12 +37,67 @@
 											name: 'type',
 											label: 'Type',
 											values: [
-												{text: '1/2 - 1/2', value: '[one_half]<br>Column 1<br>[/one_half]<br><br>[one_half_last]<br>Column 2<br>[/one_half_last]<br><br>'}
+												{
+													text: '1/2 - 1/2',
+													value: '[one_half]<br>Column 1<br>[/one_half]<br><br>' +
+													'[one_half_last]<br>Column 2<br>[/one_half_last]<br><br>'
+												},
+												{
+													text: '1/3 - 1/3 - 1/3',
+													value: '[one_third]<br>Column 1<br>[/one_third]<br><br>' +
+													'[one_third]<br>Column 2<br>[/one_third]<br><br>' +
+													'[one_third_last]<br>Column 3<br>[/one_third_last]<br><br>'
+												},
+												{
+													text: '1/4 - 1/4 - 1/4 - 1/4',
+													value: '[one_fourth]<br>Column 1<br>[/one_fourth]<br><br>' +
+													'[one_fourth]<br>Column 2<br>[/one_fourth]<br><br>' +
+													'[one_fourth]<br>Column 3<br>[/one_fourth]<br><br>' +
+													'[one_fourth_last]<br>Column 4<br>[/one_fourth_last]<br><br>'
+												},
+												{
+													text: '1/3 - 2/3',
+													value: '[one_third]<br>Column 1<br>[/one_third]<br><br>' +
+													'[two_third_last]<br>Column 2<br>[/two_third_last]<br><br>'
+												},
+												{
+													text: '2/3 - 1/3',
+													value: '[two_third]<br>Column 1<br>[/two_third]<br><br>' +
+													'[one_third_last]<br>Column 2<br>[/one_third_last]<br><br>'
+												},
+												{
+													text: '1/4 - 3/4',
+													value: '[one_fourth]<br>Column 1<br>[/one_fourth]<br><br>' +
+													'[three_fourth_last]<br>Column 2<br>[/three_fourth_last]<br><br>'
+												},
+												{
+													text: '3/4 - 1/4',
+													value: '[three_fourth]<br>Column 1<br>[/three_fourth]<br><br>' +
+													'[one_fourth_last]<br>Column 2<br>[/one_fourth_last]<br><br>'
+												},
+												{
+													text: '1/4 - 1/4 - 1/2',
+													value: '[one_fourth]<br>Column 1<br>[/one_fourth]<br><br>' +
+													'[one_fourth]<br>Column 2<br>[/one_fourth]<br><br>' +
+													'[one_half_last]<br>Column 3<br>[/one_half_last]<br><br>'
+												},
+												{
+													text: '1/4 - 1/2 - 1/4',
+													value: '[one_fourth]<br>Column 1<br>[/one_fourth]<br><br>' +
+													'[one_half]<br>Column 2<br>[/one_half]<br><br>' +
+													'[one_fourth_last]<br>Column 3<br>[/one_fourth_last]<br><br>'
+												},
+												{
+													text: '1/2 - 1/4 - 1/4',
+													value: '[one_half]<br>Column 1<br>[/one_half]<br><br>' +
+													'[one_fourth]<br>Column 2<br>[/one_fourth]<br><br>' +
+													'[one_fourth_last]<br>Column 3<br>[/one_fourth_last]<br><br>'
+												}
 											]
 										}
 									],
 									onsubmit: function(e) {
-										editor.insertContent('<h' + e.data.level + ' id="' + e.data.id + '">' + e.data.title + '</h' + e.data.level + '>');
+										editor.insertContent(e.data.type);
 									}
 								});
 							}
@@ -50,61 +105,150 @@
 					]
 				},
 				{
-					text: 'Menu item II',
-					value: 'Text from menu item II',
+					text: 'Buttons',
 					onclick: function() {
-						editor.insertContent(this.value());
-					},
+						var teks = editor.selection.getContent({format: 'text'});
+						editor.windowManager.open({
+							title: 'Insert button',
+							body: [
+								{
+									type: 'listbox',
+									name: 'color',
+									label: 'Color',
+									values: [
+										{text: 'red', value: 'red'},
+										{text: 'bittersweet', value: 'bittersweet'},
+										{text: 'coral', value: 'coral'},
+										{text: 'orange', value: 'orange'},
+										{text: 'celery', value: 'celery'},
+										{text: 'Blue', value: 'blue'},
+										{text: 'cerulean', value: 'cerulean'},
+										{text: 'eastern', value: 'eastern'},
+										{text: 'terracotta', value: 'terracotta'},
+										{text: 'pink', value: 'pink'},
+										{text: 'cranberry', value: 'cranberry'},
+										{text: 'sunset', value: 'sunset'}
+									]
+								},
+								{
+									type: 'listbox',
+									name: 'size',
+									label: 'Size',
+									values: [
+										{text: 'small', value: 'small'},
+										{text: 'medium', value: 'medium'},
+										{text: 'large', value: 'large'}
+									]
+								},
+								{
+									type: 'textbox',
+									name: 'link',
+									label: 'Link'
+								},
+								{
+									type: 'listbox',
+									name: 'target',
+									label: 'Target',
+									values: [
+										{text: '', value: ''},
+										{text: '_blank', value: '_blank'},
+										{text: '_self', value: '_self'},
+										{text: '_parent', value: '_parent'},
+										{text: '_top', value: '_top'}
+									]
+								},
+							],
+							onsubmit: function(e) {
+								var shortcode = '[button';
+								$.each(e.data, function(index, val) {
+									if (typeof val !== 'undefined') {
+										shortcode += ' ' + index + '="' + val + '"';
+									}
+								});
+								shortcode += ']';
+								if (teks) {
+									shortcode += teks;
+								} else {
+									shortcode += 'button text';
+								}
+								shortcode += '[/button]';
+								editor.insertContent(shortcode);
+							}
+						});
+					}
+				},
+				{
+					text: 'Lists',
 					menu: [
 						{
-							text: 'First submenu item',
-							value: 'Text from sub sub menu',
-							onclick: function(e) {
-								e.stopPropagation();
-								editor.insertContent(this.value());
+							text: 'bullet',
+							onclick: function() {
+								var lists = '<ul><li>List item 1</li><li>List item 2</li><li>List item 3</li></ul>';
+								editor.insertContent('[list type="bullet"]' + lists + '[/list]<br><br>');
 							}
 						},
 						{
-							text: 'Second submenu item',
-							value: 'Text from sub sub menu',
-							onclick: function(e) {
-								e.stopPropagation();
-								editor.insertContent(this.value());
+							text: 'tick',
+							onclick: function() {
+								var lists = '<ul><li>List item 1</li><li>List item 2</li><li>List item 3</li></ul>';
+								editor.insertContent('[list type="tick"]' + lists + '[/list]<br><br>');
+							}
+						},
+						{
+							text: 'cross',
+							onclick: function() {
+								var lists = '<ul><li>List item 1</li><li>List item 2</li><li>List item 3</li></ul>';
+								editor.insertContent('[list type="cross"]' + lists + '[/list]<br><br>');
+							}
+						},
+						{
+							text: 'plus',
+							onclick: function() {
+								var lists = '<ul><li>List item 1</li><li>List item 2</li><li>List item 3</li></ul>';
+								editor.insertContent('[list type="plus"]' + lists + '[/list]<br><br>');
 							}
 						}
 					]
 				},
 				{
-					text: 'Buttons',
-					value: 'Text from menu item III',
+					text: 'Message Boxes',
 					onclick: function() {
+						var teks = editor.selection.getContent({format: 'text'});
 						editor.windowManager.open({
-							title: 'Insert button',
+							title: 'Insert message box',
 							body: [
+								{
+									type: 'listbox',
+									name: 'type',
+									label: 'Type',
+									values: [
+										{text: 'info', value: 'info'},
+										{text: 'success', value: 'success'},
+										{text: 'error', value: 'error'},
+										{text: 'warning', value: 'warning'}
+									]
+								},
 								{
 									type: 'textbox',
 									name: 'title',
-									label: 'Your title'
-								},
-								{
-									type: 'textbox',
-									name: 'id',
-									label: 'Header anchor'
-								},
-								{
-									type: 'listbox', 
-									name: 'color', 
-									label: 'Color', 
-									'values': [
-										{text: 'Red', value: 'red'},
-										{text: 'Bittersweet', value: 'bittersweet'},
-										{text: 'Coral', value: 'coral'},
-										{text: 'Orange', value: 'orange'}
-									]
+									label: 'Title'
 								}
 							],
-							onsubmit: function( e ) {
-								editor.insertContent( '<h' + e.data.level + ' id="' + e.data.id + '">' + e.data.title + '</h' + e.data.level + '>');
+							onsubmit: function(e) {
+								var shortcode = '[message';
+								$.each(e.data, function(index, val) {
+									if (typeof val !== 'undefined') {
+										shortcode += ' ' + index + '="' + val + '"';
+									}
+								});
+								shortcode += ']';
+								if (teks) {
+									shortcode += teks;
+								} else {
+									shortcode += 'message text';
+								}
+								shortcode += '[/message]';
+								editor.insertContent(shortcode);
 							}
 						});
 					}
@@ -112,4 +256,4 @@
 			]
 		});
 	});
-})();
+})(jQuery);
